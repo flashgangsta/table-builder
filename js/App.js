7abccff9c6d8cc496c2d2362cbd47da22dfd2c3e
Sergey Krivtsov <sergeykv@alarstudios.com>
2022-06-09 17:30:13 +0200

    Api server requests emulation

--- js/App.js
+++ js/App.js
@@ -1,6 +1,18 @@
+import {Api} from "./api/Api.js";
+
 export class App {
     constructor() {
-        document.querySelector("body").textContent = "Welcome to the Quake III Arena!";
+        const elBody = document.querySelector("body");
+        const button = document.createElement("button");
+        elBody.textContent = "Welcome to the Quake III Arena!";
+        button.textContent = "test";
+
+        button.addEventListener("click", (event) => {
+            Api.get(Api.URL_TAGS).then((response) => {
+                console.log(response);
+            });
+        });
+        elBody.append(button);
     }
 }
 
---
+++ js/base/ElementBase.js
@@ -0,0 +1,5 @@
+export class ElementBase extends HTMLElement() {
+    constructor() {
+        super();
+    }
+}
